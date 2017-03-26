3e92d0cd8fa7ed94a757702430760781e518a1fe
AigleCatalan <edmondkge@googlemail.com>
2017-03-26 09:48:59 +0200

    change the title to Working on Function AJAX-etc...

--- index.php
+++ index.php
@@ -158,7 +158,7 @@ position:fixed;
 </div>
 
 <div id="main">
-  <h2>Working on Function overlay</h2>
+  <h2>Working on Function AJAX-DATA- In overlay</h2>
   <p>Click on the element below to open the side navigation menu, and push this content to the right. Notice that we add a black see-through background-color to body when the sidenav is opened.</p>
    <p>Notice that we add a black see-through background-color to body when the sidenav is opened.</p>
     <p>Click on the element below to open the side navigation menu, and push this content to the right. Notice that we add a black see-through background-color to body when the sidenav is opened.</p>
