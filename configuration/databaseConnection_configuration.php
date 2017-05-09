a670ab0145388ecf6d4f131ced79bcaa5289b040
ulrichkevin <kevin_ulrich2@yahoo.fr>
2017-05-09 22:42:05 +0200

    sample

--- configuration/databaseConnection_configuration.php
+++ configuration/databaseConnection_configuration.php
@@ -1,8 +1,8 @@
 <?php
 
 // Database Connection
-$connect = mysql_connect ( "Localhost", "root", "" ) or die ( "could not connect to database" );
-mysql_select_db ( 'hotelverw' ) or die ( "could not find the database!" . mysqli_connect_error () );
+$connect = mysql_connect ( "localhost", "root", "" ) or die ( "could not connect to database" );
+mysql_select_db ( 'cloudteldb' ) or die ( "could not find the database!" . mysqli_connect_error () );
 
 ?>
 
