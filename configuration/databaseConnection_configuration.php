795e2b398a2e3ec54fbb41c423a9fbd047a70d86
AigleCatalan <edmondkge@googlemail.com>
2017-05-16 13:32:57 +0200

    Changing to PDO was not successful

--- configuration/databaseConnection_configuration.php
+++ configuration/databaseConnection_configuration.php
@@ -1,7 +1,7 @@
 <?php
 
 // Database Connection
-$connect = mysql_connect ( "Localhost", "root", "" ) or die ( "could not connect to database" );
+$connect = mysql_connect ( "localhost", "root", "" ) or die ( "could not connect to database" );
 mysql_select_db ( 'cloudteldb' ) or die ( "could not find the database!" . mysqli_connect_error () );
 
 ?>
