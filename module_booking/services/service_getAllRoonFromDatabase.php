845e97dbe45df42cad4068d4b6d71ac79333362f
AigleCatalan <edmondkge@googlemail.com>
2017-05-16 13:50:08 +0200

    my branch is uptodate with pdo driver

--- module_booking/services/service_getAllRoonFromDatabase.php
+++ module_booking/services/service_getAllRoonFromDatabase.php
@@ -2,14 +2,14 @@
 include "././configuration/databaseConnection_configuration.php";
 
 $roomTable = '';
-$arrayRoomData  = array(); 
-$roomData = mysql_query ( "SELECT * FROM object" ) or die ( "MySQL-Error" . mysql_error () );
-$RoomNum_rows = mysql_num_rows ( $roomData );
-//echo $roomData;
+$arrayRoomData = array();
 
-while ( $k_row = mysql_fetch_assoc ( $roomData ) ) {
-	array_push($arrayRoomData, $k_row ['description']);
-	$roomTable .= '<tr><td>'.$k_row ['description'].'</td></tr>';
+$stmt = $pdo->prepare("SELECT *FROM object");
+$stmt->execute();
+$RoomNum_rows = $stmt->rowCount();
+
+while ($row = $stmt->fetch()) {
+    array_push($arrayRoomData, $row ['description']);
+    $roomTable .= '<tr><td>' . $row ['description'] . '</td></tr>';
 }
-
-?>
+$pdo = null;
--- module_booking/services/service_getDataFromDefinePeriod.php
+++ module_booking/services/service_getDataFromDefinePeriod.php
@@ -1,11 +1,10 @@
 <?php
 include "../../configuration/databaseConnection_configuration.php";
-header ( 'content-type: application/json' );
+header('content-type: application/json');
 
- $departure = $_POST['startDate'];
- $arivate = $_POST['enddate'];
-
-$query = "SELECT
+$departure = $_POST['startDate'];
+$arrival = $_POST['enddate'];
+$stmt = $pdo->prepare("SELECT
   client.name,
   reservationposition.arrival,
   reservationposition.departur,
@@ -15,32 +14,27 @@ FROM reservationposition
   INNER JOIN object ON reservationposition.object_objectId = object.objectId
   INNER JOIN reservation ON reservation.reservationId = reservationposition.reservation_reservationId
   INNER JOIN client ON client.clientId = reservation.client_clientId
-  WHERE reservationposition.arrival BETWEEN '$departure' AND '$arrival'
+  WHERE reservationposition.arrival BETWEEN :departur AND :arrival
 OR
-reservationposition.departur BETWEEN '$departure' AND '$arrival'
-";
+reservationposition.departur BETWEEN :departur AND :arrival
+OR
+DATEDIFF(reservationposition.departur, reservationposition.arrival) > 28");
+$stmt->execute(array(':arrival' => $arrival, ':departur' => $departure, ':arrival' => $arrival, ':departur' => $departure));
 
-$mysqlQuuery = mysql_query ( $query );
-
-$result = array ();
+$result = array();
 
 // return a json datei with all the Reservations
-while ( $row = mysql_fetch_assoc ( $mysqlQuuery ) ) {
+while ($row = $stmt->fetch()) {
 
-	$rowResult = array();
+    $rowResult = array();
     $rowResult ['objectId'] = $row ['objectId'];
     $rowResult ['objectDescription'] = $row ['description'];
     $rowResult ['kName'] = $row ['name'];
     $rowResult ['arrivalDate'] = $row ['arrival'];
     $rowResult ['departureDate'] = $row ['departur'];
-	
-	$result [] = $rowResult;
+
+    $result [] = $rowResult;
 }
+print_r(json_encode($result));
+$pdo = null;
 
-// create the json-Structur
-print_r ( json_encode ( $result ) );
-
-// close the mysql-connection
-mysql_close ( $connect );
-
-?>
