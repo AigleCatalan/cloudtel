d319ba76f7d16b5bf4b6916941c27d5043f16213
ulrichkevin <ulrichmadrid@yahoo.fr>
2017-05-19 10:29:07 +0200

    Update databaseConnection_configuration.php

--- configuration/databaseConnection_configuration.php
+++ configuration/databaseConnection_configuration.php
@@ -1,8 +1,9 @@
 <?php
-
 // Database Connection
-$connect = mysql_connect ( "localhost", "root", "" ) or die ("could not connect to database");
-mysql_select_db ( "cloudteldb" ) or die ( "could not find the database!" . mysqli_connect_error());
-
-?>
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=cloudteldb', "root", "");
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
 
